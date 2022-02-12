8716a278b7e3333bc9e1a7c26df965c504cb05be
Magnus <magn902m@stud.kea.dk>
2022-02-12 18:50:05 +0100

    got some thing show

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function start() {
 }
 
 function loadJSON() {
-  console.log("loadJSON");
+  //   console.log("loadJSON");
   fetch("students.json")
     .then((response) => response.json())
     .then((jsonData) => {
@@ -24,12 +24,38 @@ function loadJSON() {
 }
 
 function prepareObjects(jsonData) {
-  console.log("prepareObjects");
+  //   console.log("prepareObjects");
+  jsonData.forEach((jsonObject) => {
+    let fullname = jsonObject.fullname;
+    let house = jsonObject.house;
+    // let gender = jsonObject.gender;
+
+    const Student = {
+      firstName: "",
+      lastName: "",
+      middleName: "",
+      nickName: "",
+      imgName: "",
+      house: "",
+    };
+    const student = Object.create(Student);
+
+    student.firstName = fullname.substring(0, fullname.indexOf(" "));
+    student.lastName = fullname.substring(fullname.lastIndexOf(" "));
+    student.middleName = fullname.substring(fullname.lastIndexOf(" "), fullname.lastIndexOf(" "));
+    student.nickName = "";
+    student.imgName = "";
+    student.house = house.charAt(0).toUpperCase() + house.substring(1).toLowerCase();
+    // student.gender = jsonObject.gender;
+
+    allStudents.push(student);
+  });
 
   displayList();
 }
+
 function displayList() {
-  console.log("displayList");
+  //   console.log("displayList");
 
   HTML.dest.innerHTML = "";
 
@@ -42,6 +68,11 @@ function displayStudent(student) {
   const clone = HTML.template.content.cloneNode(true);
 
   clone.querySelector("[data-field=first_name]").textContent = student.firstName;
+  clone.querySelector("[data-field=last_name]").textContent = student.lastName;
+  clone.querySelector("[data-field=middle_name]").textContent = student.middleName;
+  clone.querySelector("[data-field=nick_name]").textContent = student.nickName;
+  clone.querySelector("[data-field=img_name]").textContent = student.imgName;
+  clone.querySelector("[data-field=house]").textContent = student.house;
 
   HTML.dest.appendChild(clone);
 }
