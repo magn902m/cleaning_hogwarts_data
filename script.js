4c65f29b8febc20cff1aa06abaf6353b416c2aba
Magnus <magn902m@stud.kea.dk>
2022-02-12 18:02:38 +0100

    basic js setup

--- script.js
+++ script.js
@@ -2,6 +2,46 @@
 
 window.addEventListener("DOMContentLoaded", start);
 
+const HTML = {};
+const allStudents = [];
+
 function start() {
   console.log("start");
+  HTML.list = document.querySelector("#list");
+  HTML.dest = document.querySelector("#list tbody");
+  HTML.template = document.querySelector("template#student");
+  loadJSON();
+}
+
+function loadJSON() {
+  console.log("loadJSON");
+  fetch("students.json")
+    .then((response) => response.json())
+    .then((jsonData) => {
+      // when loaded, prepare objects
+      prepareObjects(jsonData);
+    });
+}
+
+function prepareObjects(jsonData) {
+  console.log("prepareObjects");
+
+  displayList();
+}
+function displayList() {
+  console.log("displayList");
+
+  HTML.dest.innerHTML = "";
+
+  allStudents.forEach(displayStudent);
+}
+
+function displayStudent(student) {
+  console.log("displayStudent");
+
+  const clone = HTML.template.content.cloneNode(true);
+
+  clone.querySelector("[data-field=first_name]").textContent = student.firstName;
+
+  HTML.dest.appendChild(clone);
 }
