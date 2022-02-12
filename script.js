fa63302e910abf3bf598f978f02210da4a238bae
Magnus <magn902m@stud.kea.dk>
2022-02-12 19:10:13 +0100

    make so i console.table instead of creating it in html

--- script.js
+++ script.js
@@ -1,44 +1,42 @@
 `use strict`;
 
-window.addEventListener("DOMContentLoaded", start);
-
-const HTML = {};
+// Array for all students
 const allStudents = [];
+
+// Student object
+const Student = {
+  firstName: "",
+  lastName: "",
+  middleName: "",
+  nickName: "",
+  imgName: "",
+  house: "",
+};
+// API url
+const urlStudentList = "https://petlatkea.dk/2021/hogwarts/students.json";
+
+window.addEventListener("DOMContentLoaded", start);
 
 function start() {
   console.log("start");
-  HTML.list = document.querySelector("#list");
-  HTML.dest = document.querySelector("#list tbody");
-  HTML.template = document.querySelector("template#student");
   loadJSON();
 }
 
-function loadJSON() {
-  //   console.log("loadJSON");
-  fetch("students.json")
-    .then((response) => response.json())
-    .then((jsonData) => {
-      // when loaded, prepare objects
-      prepareObjects(jsonData);
-    });
+async function loadJSON() {
+  const jsonData = await fetch(urlStudentList);
+  studentData = await jsonData.json();
+  console.table(studentData);
+
+  makeStudents();
 }
 
-function prepareObjects(jsonData) {
-  //   console.log("prepareObjects");
-  jsonData.forEach((jsonObject) => {
-    let fullname = jsonObject.fullname;
-    let house = jsonObject.house;
-    // let gender = jsonObject.gender;
-
-    const Student = {
-      firstName: "",
-      lastName: "",
-      middleName: "",
-      nickName: "",
-      imgName: "",
-      house: "",
-    };
+function makeStudents() {
+  console.log("makeStudents");
+  studentData.forEach((elm) => {
     const student = Object.create(Student);
+    let fullname = elm.fullname;
+    let house = elm.house;
+    // let gender = elm.gender;
 
     student.firstName = fullname.substring(0, fullname.indexOf(" "));
     student.lastName = fullname.substring(fullname.lastIndexOf(" "));
@@ -46,33 +44,10 @@ function prepareObjects(jsonData) {
     student.nickName = "";
     student.imgName = "";
     student.house = house.charAt(0).toUpperCase() + house.substring(1).toLowerCase();
-    // student.gender = jsonObject.gender;
+    // student.gender = elm.gender;
 
     allStudents.push(student);
   });
 
-  displayList();
-}
-
-function displayList() {
-  //   console.log("displayList");
-
-  HTML.dest.innerHTML = "";
-
-  allStudents.forEach(displayStudent);
-}
-
-function displayStudent(student) {
-  console.log("displayStudent");
-
-  const clone = HTML.template.content.cloneNode(true);
-
-  clone.querySelector("[data-field=first_name]").textContent = student.firstName;
-  clone.querySelector("[data-field=last_name]").textContent = student.lastName;
-  clone.querySelector("[data-field=middle_name]").textContent = student.middleName;
-  clone.querySelector("[data-field=nick_name]").textContent = student.nickName;
-  clone.querySelector("[data-field=img_name]").textContent = student.imgName;
-  clone.querySelector("[data-field=house]").textContent = student.house;
-
-  HTML.dest.appendChild(clone);
+  console.table(allStudents);
 }
